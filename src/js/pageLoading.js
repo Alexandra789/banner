714b1b8687e98e53d8a5c5eae3e04fecd858c5ea
Alexandra1234 <alexandra.zakovinko>
2022-11-25 04:32:51 +0300

    fix loader in progress

--- src/js/pageLoading.js
+++ src/js/pageLoading.js
@@ -1,4 +1,6 @@
-window.onload = function (){
+function ready() {
     document.querySelector('.loader').style.display = "none";
     document.querySelector('.banner').style.display = "block";
 }
+
+window.addEventListener('load', ready)
