98038617fd6672104959153f231637525c47f610
Alexandra1234 <alexandra.zakovinko>
2022-11-26 02:47:21 +0300

    add background color and fix close button icon

--- src/js/pageLoading.js
+++ src/js/pageLoading.js
@@ -1,6 +1,5 @@
-function ready() {
+window.onload = function () {
     document.querySelector('.loader').style.display = "none";
     document.querySelector('.banner').style.display = "block";
+    document.querySelector('body').style.backgroundColor = "#212634";
 }
-
-window.addEventListener('load', ready)
